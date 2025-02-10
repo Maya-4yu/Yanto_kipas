6a84fdc0cea5897b5b8452e34e82628ee608bf45
Yanto_kipas <maya.emfat@gmail.com>
2025-02-10 13:31:51 +0700

    .

--- Ctrl.strukturphp/percabangan.php
+++ Ctrl.strukturphp/percabangan.php
@@ -1,9 +1,5 @@
-//1 kondisi dengan statment IF
 <?php
     $x = 5;
     if($x % 5 ==0)
         echo "Bilangan $x adalah kelipatan 5";
 ?>
-//Struktur kontrol if memeriksa isi kondisi, dimana jika kondisi terpenuhi
-atau bernilai  benar(TRUE), maka ia akan menjalankan perintah-perintah
-yang ada dalam blok.
